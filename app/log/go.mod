4c8141380f3d8983b31ac03e35fd976fd759d831
wifi32767 <wifi32767@qq.com>
2025-07-17 19:23:47 +0800

    feat: add log receiver as a tiny service

---
+++ app/log/go.mod
@@ -0,0 +1,3 @@
+module github.com/wifi32767/TikTokMall/app/log
+
+go 1.23.0
---
+++ app/log/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+
+	"github.com/wifi32767/TikTokMall/common/logger"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://admin:123456@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"log", // name
+		true,  // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	var forever chan struct{}
+
+	r := logger.NewReceiver(ch, &q)
+	r.SetOutput(log.Writer())
+	r.Run()
+
+	<-forever
+}
